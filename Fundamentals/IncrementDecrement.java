87c8ab2967cb5449604733c035098a9bb60b8ce6
Rajesha T <rajeshxyash@gmail.com>
2021-12-15 20:07:40 +0530

    update

---
+++ Fundamentals/IncrementDecrement.java
@@ -0,0 +1,22 @@
+package fundamentals;
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;
+		System.out.println(a--);
+		System.out.println(a+++--b);
+//		System.out.println(a--+++b);
+		System.out.println(a++-++b);
+		
+//		int a = 60, b = 80;
+//		if(++a > 60 || b++ > 80) {
+//			System.out.println("Inside if ");
+//		}else {
+//			System.out.println("Inside else");
+//		}
+//		System.out.println("a " + a + " b " + b);
+	}
+
+}
