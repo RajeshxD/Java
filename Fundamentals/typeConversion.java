dd9cbe0a9946a33a1bd462f9d5749ffd35f4f0d4
Rajesha T <rajeshxyash@gmail.com>
2021-12-17 21:26:56 +0530

    update

---
+++ Fundamentals/typeConversion.java
@@ -0,0 +1,26 @@
+package fundamentals;
+
+public class typeConversion {
+
+	public static void main(String[] args) {
+//		char ch = 'a';
+//		int i = ch;
+//
+//		
+////		explicit conversion
+//		ch =(char)i; 
+//		ch = (char)(ch + 1);
+//		i = ch;
+		
+//		short s = 17;
+//		int i = s;
+//		s =(short)i;
+		
+//		int i =(int)10.6;
+//		
+//
+		
+		System.out.println(2/5.0);
+	}
+
+}
